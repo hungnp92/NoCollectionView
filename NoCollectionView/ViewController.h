bb3b20ca6fd0e9a05f1c3a244e907800ee9fc465
Nguyen Phi Hung <MacBook@MacBooks-MacBook-Pro-2.local>
2014-10-05 14:25:32 +0700

    Initial Commit

---
+++ NoCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NoCollectionView
+//
+//  Created by Nguyen Phi Hung on 10/5/14.
+//  Copyright (c) 2014 NguyenPhiHung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NoCollectionView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NoCollectionView
+//
+//  Created by Nguyen Phi Hung on 10/5/14.
+//  Copyright (c) 2014 NguyenPhiHung. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
